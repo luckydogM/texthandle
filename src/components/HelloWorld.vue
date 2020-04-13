3573272c96f0b41cdfd4f66ee7f222bf2b3255f8
luckdogM <1092608864@qq.com>
2020-04-13 13:02:31 +0800

    增加示例图

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,82 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p style="text-align:left">源数据</p>
+    <el-input type="textarea" v-model="text"></el-input>
+    <div>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="pages"
+        :page-size="1"
+        @current-change="changePage"
+        :current-page="page"
+      >
+      </el-pagination>
+    </div>
+
+    <div class="fl">
+      <el-tree :data="dir" :props="defaultProps" default-expand-all :expand-on-click-node="false" @node-click="handleNodeClick"></el-tree>
+    </div>
+    <div class="fr" v-html="manualInfo" ref="scrollContainer">
+    </div>
+
   </div>
 </template>
 
 <script>
+import util from './common/util.js'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      page:1,
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      },
+      ref: null,
+      text: '<h1>h1标题</h1><h2>h2标题</h2><p>helloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworldhelloworld</p><h3>h3标题</h3><p>121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213121322131213221312132213</p><h2>h2标题222</h2><p>11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111</p><h1>h1标题第二段</h1><h3>第二个h2标题</h3><h4>h3标题</h4><h5>h5标题</h5>'
+    }
+  },
+  computed: {
+    pages() {
+      return this.manual.length
+    },
+    manualInfo() {
+      return this.manual[this.page-1]
+    },
+    obj() {
+      return util.textHandle.getTextInfo(this.text)
+    },
+    manual() {
+      return this.obj.manual
+    },
+    dir() {
+      return this.obj.dir
+    }
+  },
+  watch: {
+    ref(target) {
+      setTimeout(()=>{
+        var container = this.$refs.scrollContainer
+        var element = container.querySelector('[ref='+ target + ']')
+        if (element) {
+          var offset = element.offsetTop
+          container.scrollTop = offset - container.offsetTop
+        }
+      },100)
+    }
+  },
+  methods: {
+    changePage(val) {
+      console.log(val);
+      this.page = val
+    },
+    handleNodeClick(data) {
+      this.page = data.page * 1
+      this.ref = data.ref
+    }
+
   }
 }
 </script>
@@ -54,5 +96,24 @@ li {
 }
 a {
   color: #42b983;
+}
+.fl {
+  float: left;
+  width: 20%;
+  border: 1px solid #ddd;
+  overflow: hidden;
+}
+.fr {
+  float: right ;
+  width: 76%;
+  text-align: left;
+  border: 1px solid #ddd;
+  padding: 0 20px;
+  height: 300px;
+  overflow: auto;
+  word-break: break-all;
+}
+p {
+  line-height: 2;
 }
 </style>
---
+++ src/components/common/util.js
@@ -0,0 +1,200 @@
+export default {
+  textHandle: {
+    targetCount: 0, //锚点计数
+    myManual: [], // 收集章节信息
+    dir: [],//收集目录信息
+    count: 0,// 计算嵌套层级 层级为2时就
+    getTextInfo (string) {
+      string = this.formatString(string)
+      var count = this.count
+      this.getList(string,null,count)
+      this.dir = this.getPage(this.myManual)
+      var relationShip = this.getRelationShip(this.dir)
+      var arrsData = this.formatData(relationShip)
+      this.dir = this.plain2Tree(arrsData)
+      var myManual = [...this.myManual]
+      var dirs = [...this.dir]
+      this.myManual = []
+      this.dir = []
+      this.targetCount = 0
+      return {
+        dir: dirs,
+        manual: myManual
+      }
+    },
+    // 字符串加锚点处理
+    formatString(string){
+      var self = this
+      var strings = string.replace(/<h.*?(?:>|\/>)/g, function(match) {
+        self.targetCount++
+        var newStr = match.slice(0,-1) + ' ref="target' + self.targetCount +'">';
+        return newStr
+      })
+      return strings
+    },
+    // 字符串分页
+    getList (str, i, count) {
+      i = i ? i : 1;
+      if (i > 6) {
+        return
+      }
+      var reg = new RegExp("<h"+i+".+?>",'g')
+      var reg2 = "</h"+i+">"
+      // 如果匹配到最外层标签
+      var arr = this.getMatch(str, reg);
+      var arr2 = this.getMatch(str, reg2);
+      // 如果最外层还没匹配到则继续向下匹配
+      if (arr.length == 0) {
+        i++;
+        this.getList(str, i,count)
+      } else {
+        // 将字符串分段
+        var afterSplite = this.splitStr(str, arr);
+        count++;
+        //遍历数组继续下一个分段
+        afterSplite.map((valueStr, index)=>{
+          // 判断当前节点下是否有子节点和父节点 并连接和子节点和父节点的关系
+          var firstHead = str.slice(arr[index],arr2[index]+reg2.length)
+          if (count < 2) {
+            this.myManual.push(firstHead)
+          }
+          this.getList(valueStr, i+1, count)  
+        })
+        if (count == 2) {
+          afterSplite[0] = this.myManual.splice(-1) + afterSplite[0]
+          this.myManual = this.myManual.concat(afterSplite)
+        }
+      }
+    },
+    // 增加分页标记
+    getPage (arr) {
+      var result = []
+      arr.map((val,index)=>{
+        var str = val
+        var mStr = ''
+        str.replace(/(<h.*?>)(.*?)(<\/h.*?>)/g,function(g, $1, $2, $3){
+          mStr += $1.slice(0,-1) + ' page="' + (index+1) +'">' + $2 + $3
+        })
+        var newArr = mStr.split('<h').map((val)=>{
+          if(val){
+            return  '<h'+val
+          }
+        })
+        newArr = newArr.filter(function(e){return e}); 
+        result.push(...newArr)
+      })
+      return result
+    },
+    // 格式化数据专用 
+    formatData (arr){
+      var data = {};
+      for (var i = 0;i < arr.length;i++){
+        var val = arr[i];
+        data[val.id] = val;
+      }
+      data[-1] = {parentId: '', name: 'root'};
+      return data;
+    },
+    // 获取目录的数据结构 //获取父子关系表
+    getRelationShip (arr) {
+      var result = []
+      arr.map((str,i)=>{
+        var hTag = str[2]
+        var parent=-1
+        var ref;
+        var page;
+        if (i != 0) {
+          for (var j=i-1;j>=0;j--) {
+            if (hTag > arr[j][2]) {
+              parent = arr[j][2]+'-'+j
+              break
+            } else if (j==0) {
+              parent = -1
+            }
+          }
+        }
+        var match1 = str.match(/page=".+?"/)
+        match1 = match1 && match1[0]
+        match1 = match1.match(/\d/)
+        page = match1 && match1[0]
+        
+        var match2 = str.match(/ref="target.+?"/)
+        match2 = match2 && match2[0]
+        match2 = match2.match(/\d/)
+        ref = match2 && 'target' + match2[0]
+
+        result.push({
+          id: hTag + '-' + i,
+          label: str.replace(/<\/?.+?\/?>/g, ''),
+          parentId: parent,
+          ref,
+          page
+        })
+      })
+      return result
+    },
+    // 父子关系表转为树状结构
+    plain2Tree (obj) {
+      let key;
+      let res = {};
+      for (key in obj) {
+        let val = obj[key];
+        var parent = val.parentId;
+        if (parent === '') {
+          res = val;
+        } else {
+          if (!obj[parent].children) {
+            obj[parent].children = [];
+          }
+          obj[parent].children.push(val);
+        }
+      }
+      return res.children;
+    },
+    //工具类begin
+    // 分段字符串 
+    splitStr (str, arr) {
+      var len = str.length
+      var result=[]
+      arr.map((val,i)=>{
+        var end = len;
+        if (i !== arr.length-1) {
+          end = arr[i+1]
+        }
+        result.push(str.slice(val, end))
+      })
+      return result
+    },
+    // 获取匹配到的值的所有下标
+    getMatch (str, reg) {
+      var result = [];
+      var arr = true;
+      if (typeof reg === 'object') {
+        while(arr){
+          arr = reg.exec(str)
+          if (arr) {
+            var index = arr.index;
+            reg.lastIndex;
+            result.push(index);
+          } else {
+            break
+          }
+        }
+      } else if (typeof reg === 'string') {
+        let index = -1;
+        var beforeIndex = 0;
+        while(arr){
+          index = str.indexOf(reg)
+          if (index !== -1) {
+            str = str.slice(index+reg.length)
+            result.push(index+beforeIndex);
+            beforeIndex = index+reg.length
+          } else {
+            break
+          }
+        }
+      }
+      return result
+    }
+  }
+}
